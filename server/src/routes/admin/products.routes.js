4b18260b237544a6cd5ccb220589a88d1b76a028
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-09-09 22:27:11 +0530

    added admin product model controller routes

--- server/src/routes/admin/products.routes.js
+++ server/src/routes/admin/products.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { handleImageUpload } from "../../controllers/admin/products.controller.js";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProducts,
+  handleImageUpload,
+} from "../../controllers/admin/products.controller.js";
 import { upload } from "../../utils/cloudinary.js";
 
 const router = express.Router();
 
 router.route("/upload-image").post(upload.single("my_file"), handleImageUpload);
+router.route("/add-product").post(addProduct);
+router.route("/edit-product/:id").put(editProduct);
+router.route("/delete-product/:id").delete(deleteProduct);
+router.route("/get-all-products").get(fetchAllProducts);
 
 export default router;
