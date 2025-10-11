540f84bce8d2b6c152867f728bf4bf1d4abb030a
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-10-11 18:55:47 +0530

    get product details controller and route

--- server/src/routes/shop/products.routes.js
+++ server/src/routes/shop/products.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getFilteredProducts } from "../../controllers/shop/products.controller.js";
+import {
+  getFilteredProducts,
+  getProductDetails,
+} from "../../controllers/shop/products.controller.js";
 
 const router = express.Router();
 
 router.route("/get-all-products").get(getFilteredProducts);
+router.route("/get-product-details/:id").get(getProductDetails);
 
 export default router;
