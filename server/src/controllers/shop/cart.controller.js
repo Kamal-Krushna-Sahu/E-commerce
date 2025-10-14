840bea9fd283c009ff1a024e57f7a415fd7ee2d0
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-10-14 20:49:23 +0530

    added fetch cart items and update cart items controllers

--- server/src/controllers/shop/cart.controller.js
+++ server/src/controllers/shop/cart.controller.js
@@ -54,6 +54,53 @@ const addToCart = async (res, res) => {
 
 const fetchCartItems = async (res, res) => {
   try {
+    const { userId } = req.params;
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "userId is mandatory !!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "item.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return res.ststus(404).json({
+        success: false,
+        message: "cart not found !!",
+      });
+    }
+
+    // check, if all cart products are still available and if any product has been deleted by the admin from database
+    const validItems = cart.items.filter(
+      (productItem) => productItem.productId
+    );
+
+    if (validItems.length < cart.items.length) {
+      cart.items = validItems;
+      await cart.save();
+    }
+
+    const populateCartItems = validItems.map((item) => ({
+      productId: item.productId._id,
+      image: item.productId.image,
+      title: item.productId.title,
+      price: item.productId.price,
+      salePrice: item.productId.salePrice,
+      quantity: item.quantity,
+    }));
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -64,6 +111,59 @@ const fetchCartItems = async (res, res) => {
 
 const updateCartItemQty = async (res, res) => {
   try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Inavlid data provided by user !!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "cart not found !!",
+      });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "cart item not present !!",
+      });
+    }
+
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.image ? item.productId.image : null,
+      title: item.title ? item.productId.title : "product not found",
+      price: item.price ? item.productId.price : null,
+      salePrice: item.salePrice ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
