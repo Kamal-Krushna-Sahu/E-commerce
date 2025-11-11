89f77a58223fba4a2a175482f8ef995bf8979834
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-11-11 17:07:15 +0530

    worked on admin orders page UI

---
+++ client/src/components/admin-view/AdminOrders.jsx
@@ -0,0 +1,50 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+function AdminOrdersView() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>All Orders</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order Id</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>123456</TableCell>
+              <TableCell>1-1-25</TableCell>
+              <TableCell>pending</TableCell>
+              <TableCell>3000/-</TableCell>
+              <TableCell>
+                <Button>View Details</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default AdminOrdersView;
--- client/src/pages/admin-view/AdminOrders.jsx
+++ client/src/pages/admin-view/AdminOrders.jsx
@@ -1,8 +1,11 @@
+import AdminOrdersView from "@/components/admin-view/AdminOrders";
 
 const AdminOrders = () => {
   return (
-    <div>AdminOrders</div>
-  )
-}
+    <div>
+      <AdminOrdersView />
+    </div>
+  );
+};
 
-export default AdminOrders
+export default AdminOrders;
